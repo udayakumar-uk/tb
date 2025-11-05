0b4c96e4e1ce9f0a849eaac8800286b0f2a9b202
udayakumar-uk <v.udayakumar97@gmail.com>
2025-11-05 14:45:50 +0530

    statistics ui page

--- statistics.php
+++ statistics.php
@@ -3,472 +3,60 @@ ob_start();
 @session_start();
 include "include/include.php";
 ?>
+
 <!DOCTYPE html>
-<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
-<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
-<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<html lang="en">
 <head>
-    <!-- Basic Page Needs
-  ================================================== -->
-	<meta charset="utf-8">
-	<title>Tobacco Board</title>
-	<meta name="description" content="Free Html5 Templates and Free Responsive Themes Designed by Kimmy | zerotheme.com">
-	<meta name="author" content="www.zerotheme.com">
-    <!-- Mobile Specific Metas
-  ================================================== -->
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <!-- CSS
-  ================================================== -->
-	<link rel="stylesheet" href="css/zerogrid.css">
-	<link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/responsive.css">
-	<link rel="stylesheet" href="css/responsiveslides.css" />
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<!--[if lt IE 8]>
-       <div style=' clear: both; text-align:center; position: relative;'>
-         <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
-           <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
-        </a>
-      </div>
-    <![endif]-->
-    <!--[if lt IE 9]>
-		<script src="js/html5.js"></script>
-		<script src="js/css3-mediaqueries.js"></script>
-	<![endif]-->
-	<!--<link href='./images/favicon.ico' rel='icon' type='image/x-icon'/>-->
-	<script src="js/jquery.min.js"></script>
-	<script src="js/responsiveslides.js"></script>
-	<script>
-    $(function () {
-      $("#slider").responsiveSlides({
-        auto: true,
-        pager: true,
-        nav: true,
-        speed: 500,
-        maxwidth: 800,
-        namespace: "centered-btns"
-      });
-    });
-  </script>
-<!--<style>
-.dropbtn {
-   /* background-color: #4CAF50;*/
-    color: white;
-  /*  padding: 16px;*/
-    font-size: 13px;
-    border: none;
-    cursor: pointer;
-}
-
-.dropdown {
-    position: relative;
-    display: inline-block;
-}
-
-.dropdown-content {
-    display: none;
-    position: absolute;
-    background-color: #003300;
-    min-width: 190px;
-    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-    z-index: 1;
-}
-
-.dropdown-content a {
-    color:#fff;
-    padding: 12px 16px;
-    text-decoration: none;
-    display: block;
-}
-
-.dropdown-content a:hover {background-color: #669900}
-
-.dropdown:hover .dropdown-content {
-    display: block;
-}
-
-.dropdown:hover .dropbtn {
-  /*  background-color: #3e8e41;*/
-}
-.bottom a{
-padding:5px;
-color:#950C16;
-font-size:12px;
-}
-.bottom{
-border-bottom:2px solid #bd6424;
-}
-.bottom1 a{
-padding:5px;
-color:#0f5f17;
-font-size:12px;
-}
-.bottom2{
-    background-color: #0f5f17;
-    border-radius: 7px;
-    width: 100%;
-    padding: 8px;
-	margin-bottom:5px;
-}
-.bottom2 a{
-padding:0 5px;
-color:#fff;
-font-size:12px;
-}
-.bottom3 {
-padding:5px;
-color:#666;
-font-size:12px;
-}
-
-.holder {
-    background-color: #eee;
-    width: 440px;
-    overflow: hidden;
-    padding: 10px;
-    font-family: Helvetica;
-    height: 205px;
-}
-.holder .mask {
-  position: relative;
-  left: 0px;
-  top: 10px;
-  width:440px;
-  height:205px;
-  overflow: hidden;
-}
-.holder ul {
-  list-style:none;
-  margin:0;
-  padding:0;
-  position: relative;
-}
-.holder ul li {
- padding: 3px 0px;
-border-bottom: 1px solid #ccc;
-}
-.holder ul li a {
-  color:#413c3c;
-  text-decoration:none;
-  font-size:12px;
-}
-.gal img{
-border: #999 5px solid;
-margin:5px 2px;
-}
-ul.list li a {
-    font-size: 13px;
-    color: #686666;
-    line-height: 25px;
-}
-.main-content article {
-    margin: 0 0 10px 0;
-    overflow: hidden;
-    position: relative;
-}
-.sidebar section {
-    margin-bottom: 0px;
-}
-.sidebar .content {
-    padding: 10px;
-}
-</style>-->    
-<style>
-.dropbtn {
-   /* background-color: #4CAF50;*/
-    color: white;
-  /*  padding: 16px;*/
-    font-size: 13px;
-    border: none;
-    cursor: pointer;
-}
-
-.dropdown {
-    position: relative;
-    display: inline-block;
-}
-
-.dropdown-content {
-    display: none;
-    position: absolute;
-    background-color: #003300;
-    min-width: 190px;
-    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-    z-index: 1;
-}
-
-.dropdown-content a {
-    color:#fff;
-    padding: 12px 16px;
-    text-decoration: none;
-    display: block;
-}
-
-.dropdown-content a:hover {background-color: #669900}
-
-.dropdown:hover .dropdown-content {
-    display: block;
-}
-
-.dropdown:hover .dropbtn {
-  /*  background-color: #3e8e41;*/
-}
-.bottom a{
-padding:5px;
-color:#950C16;
-font-size:12px;
-}
-.bottom{
-border-bottom:2px solid #bd6424;
-}
-.bottom1 a{
-padding:5px;
-color:#0f5f17;
-font-size:12px;
-}
-.bottom2{
-    background-color: #0f5f17;
-    border-radius: 7px;
-    width: 100%;
-    padding: 8px;
-	margin-bottom:5px;
-}
-.bottom2 a{
-padding:0 5px;
-color:#fff;
-font-size:12px;
-}
-.bottom3 {
-padding:5px;
-color:#666;
-font-size:12px;
-}
-
-.navbar-inverse .navbar-nav > .active > a, .navbar-inverse .navbar-nav > .active > a:focus, .navbar-inverse .navbar-nav > .active > a:hover {
-    color: #fff;
-    background-color: transparent;
-}
-.navbar-nav > li > a {
-    padding-top: 1px;
-    padding-bottom: 1px;
-}
-.navbar-collapse {
-    padding-right: 0;
-    padding-left: 0px;
-	}
-	.container-fluid {
-    padding-right: 0;
-    padding-left: 0;
-    margin-right: auto;
-    margin-left: auto;
-}
-
-.navbar {
-    position: relative;
-    min-height: 40px;
-    margin-bottom: 2px;
-    border: 1px solid transparent;
-       
-}
-.navbar-inverse .navbar-nav > li > a{
-color:#fff;
-}
-
-.nav > li > a {
-    position: relative;
-    display: block;
-    padding: 2px 6px;
-	}
-.dropdown-menu {
-    position: absolute;
-    top: 100%;
-    left: 0;
-    z-index: 1000;
-    display: none;
-    float: left;
-    min-width: 160px;
-    padding: 5px 0;
-    margin: 2px 0 0;
-    font-size: 14px;
-    text-align: left;
-    list-style: none;
-    background-color: #003300;
-    -webkit-background-clip: padding-box;
-    background-clip: padding-box;
-    border: 1px solid #ccc;
-    border: 1px solid rgba(0,0,0,.15);
-    border-radius: 4px;
-     -webkit-box-shadow: 0 6px 12px rgba(0,0,0,.175);
-    box-shadow: 0 6px 12px rgba(0,0,0,.175);
-}	
-.dropdown-menu > li > a:focus, .dropdown-menu > li > a:hover {
-    color: #fff;
-    text-decoration: none;
-    background-color: #669900;
+	<title> Statistics | Tobacco Board</title>
 	
-}
-.dropdown-menu > li > a{
-color:#fff;
-}
-.navbar-inverse .navbar-nav > .open > a, .navbar-inverse .navbar-nav > .open > a:focus, .navbar-inverse .navbar-nav > .open > a:hover {
-    color: #fff;
-    background-color: #003300;
-}
-.h2, h2 {
-    font-size: 15px;
-	font-weight:bold;
-	padding:5px;
-}
+	<?php include "head.php"; ?>
 
-.navbar-inverse {
-    background-color: transparent;
-    border-color: #fff;
-}
-.navbar-toggle {
-background:#333;
-}
-.headtitles {
-    color: #a05404;
-    font-size: 20px;
-    line-height: 40px;
-}
-/*.topnav {
-  overflow: hidden;
-  background-color: #333;
-  width:100%;
-}
-
-.topnav a {
-  float: left;
-  display: block;
-  color: #f2f2f2;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-}
-
-.topnav a:hover {
-  background-color: #ddd;
-  color: black;
-}
-
-.topnav .icon {
-  display: none;
-}
-
-@media screen and (max-width: 600px) {
-  .topnav a:not(:first-child) {display: none;}
-  .topnav a.icon {
-    float: right;
-    display: block;
-  }
-}
-
-@media screen and (max-width: 600px) {
-  .topnav.responsive {position: relative;}
-  .topnav.responsive .icon {
-    position: absolute;
-    right: 0;
-    top: 0;
-  }
-  .topnav.responsive a {
-    float: none;
-    display: block;
-    text-align: left;
-  }
-
-}*/
-.breadcrumb {
-    padding: 8px 15px;
-    margin-bottom: 5px;
-    list-style: outside none none;
-    background-color:#fff;
-    border-radius: 4px;
-}
-
-</style>
 </head>
 <body>
+
 <?php include "tb_header.php"; ?>		
-<!--------------Content--------------->
-<section id="content">
-	<div class="zerogrid">
-		<div class="row block">
-		<?php include "tb_leftmenu.php"; ?>
-			<div class="main-content col11">
-				<article>
-				<!--<h2 style="border-bottom:2px solid #c66f2f">ABOUT US</h2>-->
-					<div class="heading">
-					<div id="breadcrumbs-four">
-	<a href="index.php" class="active">Home</a>
-	<a href="#">Statistics</a>
-	
-</div>
-                    <!--<p><span class="navclass"> <a href="index.php" style="color:#FFFFFF" >Home</a></span>  &nbsp; &raquo;&nbsp; 
-	 	                   <span class="navclass"><a href="#" style="color:#FFFFFF"> Auction Procedures</a></span>  &nbsp; &raquo;&nbsp; 
-                           <span class="navclass"><a href="#" style="color:#FFFFFF"> Board Activities</a></span>
-                      </p>-->
-						<!--<h2><a href="#">Sample post with, links, paragraphs and comments</a></h2>-->
-						<!--<p class="info">>>> Posted by Admin - 01/01/2012 - 0 Comments</p>-->
-					</div>
-					<div class="content" style="padding-top:15px; text-align:justify">
-                          <?php // echo $rowc['content'] ?>
-			<div class="innercontent">
-            <div id="innermenu" class="innermenbox" style="display:none;"> </div>            
-            <div class="rightcorner1">
-              <div class="innercontent">
-                <div>
-                  <div>
-                    <div>
-                      <div>
-                        <table width="100%" border="0">
-                          <?php
-						  if(!empty($_GET['stype']))
-						  $stype=$_GET['stype'];
-						  else
-						  $stype='';
-		$max_recs_per_page=30;
-		if(!empty($_GET['ar']) && $_GET['ar']==1)
-		$archive="where archive=1";
-		else
-		$archive="where archive=0";
-		
-		if(!empty($_GET['stype']))
-		$archive.=" and stype='".$stype."'";
-		
-		$select=executework("select * from tob_statistics ".$archive." order by id desc");
-		$count=@mysqli_num_rows($select);
-		$row=@mysqli_fetch_array($select);
-if ($count > 0)
-{
-      ?>
-                          <tr>
-                            <td><table width="95%" border="0" align="center">
-                                <?php
-	if (empty($_GET['page_index']))
-	{
+
+
+<div id="main-content">
+  <div id="content" class="container">
+
+    <?php
+    if(!empty($_GET['stype']))
+     $stype=$_GET['stype'];
+    else
+      $stype='';
+      $max_recs_per_page=30;
+    if(!empty($_GET['ar']) && $_GET['ar']==1)
+      $archive="where archive=1";
+    else
+      $archive="where archive=0";
+    
+    if(!empty($_GET['stype']))
+      $archive.=" and stype='".$stype."'";
+    
+    $select=executework("select * from tob_statistics ".$archive." order by id desc");
+    $count=@mysqli_num_rows($select);
+    $row=@mysqli_fetch_array($select);
+
+    if ($count > 0){ ?>
+
+    <?php
+	if (empty($_GET['page_index'])) {
 		$page_index=1;
 	}	
-	else
-	{
+	else {
 		$page_index=$_GET['page_index'];
 	}
 	$total_recs = $count;
 	$pages = $count / $max_recs_per_page; 
-	if ($pages < 1)
-	{ 
+
+	if ($pages < 1) { 
 		$pages = 1; 
 	}
-	if ($pages / (int) $pages <> 1)
-	{ 
+	if ($pages / (int) $pages <> 1) { 
 		$pages = (int) $pages + 1; 
 	} 
-	else
-	{ 
+	else { 
 		$pages = $pages; 
 	}
 	$page12=(int) $page_index;
@@ -478,99 +66,49 @@ if ($count > 0)
 	$select1= executework("select * from tob_statistics $archive order by id desc LIMIT $pagenow1, $max_recs_per_page");
 	$count1 = @mysqli_num_rows($select1);
 	
-	if($pages > 1)
-	{
-	?>
-                                <tr>
-                                  <td colspan="2" align="right"><font size="2" face="Arial, Helvetica, sans-serif"><strong>Page&nbsp;
-                                          <?php
-	  for($im=1;$im<=$pages;$im++)
-	  {
-	  		if($page12 != $im)
-			{
-				?>
-                                          <a href="statistics.php?page_index=<?php echo "$im" ?>&amp;ar=<?php echo $_GET['ar']?>&stype=<?php echo $stype; ?>" class="hlink1"><?php echo "$im" ?></a>&nbsp;
-                                          <?php
-			}
-			else
-			{
-					?>
-                                          <font color="red"><?php echo "$im" ?></font>&nbsp;
-                                          <?php
-			}
-		}
-	?>
-                                  </strong></font></td>
-                                </tr>
-                                <?php
-	}
-	?>
-                            </table></td>
-                          </tr>
-                          <tr>
-                            <td><table width="95%" border="0" align="center" cellpadding="1" cellspacing="1">
-                                <tr>
-                                  <td align="justify" style="padding:10px;"><ul>
-                                      <?php
-		  	while($rows=@mysqli_fetch_array($select1))
-			{
+
+if($pages > 1) { ?>
+      <div class="d-flex align-items-center justify-content-between">
+        <h4 class="sub-title no-dash mb-0">Statistics</h4>
+        <ul class="pagination">
+    <?php
+	    for($im=1;$im<=$pages;$im++) {
+	  		if($page12 != $im) { ?>
+            <?php
+              $q_stype = isset($_GET['stype']) ? urlencode($_GET['stype']) : '';
+              $q_ar = isset($_GET['ar']) ? urlencode($_GET['ar']) : '';
+            ?>
+            <li class="page-item"><a class="page-link hlink1" href="statistics.php?page_index=<?php echo $im; ?>&amp;stype=<?php echo $q_stype; ?>&amp;ar=<?php echo $q_ar; ?>"> <?php echo $im; ?> </a></li>
+          <?php } else { ?>
+            <li class="page-item active" aria-current="page"> <span class="page-link"><?php echo "$im" ?></span> </li>
+          <?php } } ?>
+          
+        </ul>
+      </div>
+    <?php } ?>
+
+
+    <ul>
+       <?php while($rows=@mysqli_fetch_array($select1)) {
 				if($rows['tfile']!="")
 				$link=$base_url."/tbdata/statisticsfiles/".$rows['tfile'];
 				else
 				$link="#";
 				$fcheck=explode(".",$rows['tfile']);
-				if(!empty($rows['tfile']))
-				{
+				if(!empty($rows['tfile'])) {
 		  ?>
-                                      <li><strong><?php echo $rows['title'];?></strong><br />
-                                          <a href="<?php echo $link?>" target="<?php if($link!="#"){?>_blank<?php }?>"><?php echo $rows['description']; if($fcheck[1]=='pdf'){?>&nbsp;<img src="tob2_imgs/pdf_icon.gif" width="12" height="15" border="0" /><?php }?></a>&nbsp;&nbsp;&nbsp;<a href="<?php echo $link?>" target="<?php if($link!="#"){?>_blank<?php }?>"><img src="tob2_imgs/newindow_icon.gif" border="0" />											
-                                        </a></li>
-          <?php	
-				}
-				else
-				{
-					echo "<li><strong>".$rows['title']."</strong><br />".$rows['description']."</li>";
-				}
-			}
-			?>
-                                  </ul></td>
-                                </tr>
-                            </table></td>
-                          </tr>
-                          <?php
-		}
-		else
-		{
-	?>
-                          <tr>
-                            <td height="90" colspan="3"><div align="center">
-                                <p>&nbsp;</p>
-                              <p>Required Information Not Available</p>
-                              <p>&nbsp;</p>
-                            </div></td>
-                          </tr>
-                          <?php
-		}
-	?>
-                          <tr>
-                            <td style="padding-left:15px;">&nbsp;</td>
-                          </tr>
-                        </table>
-                      </div>
-                    </div>
-                    </div>
-                </div>
-              </div>
-              </div>
-              </div>
-					</div>
-				<!--	<div class="footer">
-						<p class="more"><a class="button" href="#">Read more >></a></p>
-					</div>-->
-				</article>
-				
-			</div>
-			
+        <li><strong><?php echo $rows['title'];?></strong><br />
+          <a href="<?php echo $link?>" target="<?php if($link!="#"){?>_blank<?php }?>"><?php echo $rows['description']; if($fcheck[1]=='pdf'){?> <img src="tob2_imgs/pdf_icon.gif" width="12" height="15" border="0" /><?php }?></a> 
+          <a href="<?php echo $link?>" target="<?php if($link!="#"){?>_blank<?php }?>"><img src="tob2_imgs/newindow_icon.gif" border="0" /></a>
+        </li>
+          <?php } else { echo "<li><strong>".$rows['title']."</strong><br />".$rows['description']."</li>"; } } ?>
+    </ul>
+
+   <?php } else { ?>
+        <h4 class="sub-title no-dash mb-0"><?php echo $stp ?></h4>
+        <div class="py-5 text-secondary">Required Information Not Available</div>
+    <?php } ?>
+
 			
 			
 		</div>
@@ -579,44 +117,6 @@ if ($count > 0)
 
 <!--------------Footer--------------->
 <?php include "tb_footer.php"; ?>
-<script>
-jQuery.fn.liScroll = function(settings) {
-	settings = jQuery.extend({
-		travelocity: 0.03
-		}, settings);		
-		return this.each(function(){
-				var $strip = jQuery(this);
-				$strip.addClass("newsticker")
-				var stripHeight = 1;
-				$strip.find("li").each(function(i){
-					stripHeight += jQuery(this, i).outerHeight(true); // thanks to Michael Haszprunar and Fabien Volpi
-				});
-				var $mask = $strip.wrap("<div class='mask'></div>");
-				var $tickercontainer = $strip.parent().wrap("<div class='tickercontainer'></div>");								
-				var containerHeight = $strip.parent().parent().height();	//a.k.a. 'mask' width 	
-				$strip.height(stripHeight);			
-				var totalTravel = stripHeight;
-				var defTiming = totalTravel/settings.travelocity;	// thanks to Scott Waye		
-				function scrollnews(spazio, tempo){
-				$strip.animate({top: '-='+ spazio}, tempo, "linear", function(){$strip.css("top", containerHeight); scrollnews(totalTravel, defTiming);});
-				}
-				scrollnews(totalTravel, defTiming);				
-				$strip.hover(function(){
-				jQuery(this).stop();
-				},
-				function(){
-				var offset = jQuery(this).offset();
-				var residualSpace = offset.top + stripHeight;
-				var residualTime = residualSpace/settings.travelocity;
-				scrollnews(residualSpace, residualTime);
-				});			
-		});	
-};
 
-$(function(){
-    $("ul#ticker01").liScroll();
-});
-</script>
-
-
-</body></html>
+</body>
+</html>
